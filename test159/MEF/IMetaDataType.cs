41005beaab5ec7bf1a26293d45866716c580188b
mohmmad.m.othman <mohmmad.m.othman@gmail.com>
2019-10-23 16:38:56 +0200

    init

---
+++ test159/MEF/IMetaDataType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+namespace ServerGame.MEF
+{
+    interface IMetaData
+    {
+        Char TypeOfMEF { get; }
+ 
+       
+    }
+}
