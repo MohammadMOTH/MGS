36ee7e1c5e273b4abd4cd74ae3b44baee680d1f9
mohmmad.m.othman <mohmmad.m.othman@gmail.com>
2019-10-23 17:41:13 +0200

    Adding Permissions Interface and Namespace

---
+++ test159/Permissions/IPermissionsUser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerGame.Permissions
+{
+    interface PermissionsUser : Permissions
+    {
+    }
+}
---
+++ test159/Permissions/IPermissionsZone.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerGame.Permissions
+{
+    interface IPermissionsZone : IPermissions
+    {
+    }
+}
