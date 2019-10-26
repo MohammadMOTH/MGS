fcc5801ea36f2903a9bff1181e2c07a0303fa21e
mohmmad.m.othman <mohmmad.m.othman@gmail.com>
2019-10-26 14:12:45 +0200

    Adding folder Program for Start Program

---
+++ test159/Program/Room/Room.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ServerGame.Interface.Permissions;
+
+namespace ServerGame.Program.Room
+{
+   public class Room : Core.Room.RoomAbstract
+    {
+        public override string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override int MaxPlayers { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override IPermissionsUser PermissionsUser { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override List<IUser> UserInRoom { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override DateTime DateStart { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override DateTime DataEnd { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override List<Ievnet> _MyEvnets { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        
+        public Room ()
+        {
+        
+
+
+        }
+        public override bool AddEventListener(Ievnet evnet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool JoinUserToRoom(IUser User)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool _AddNewUser(IUser User)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool _CheckBlackList(IUser InputUserToSendData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool _CheckPermissionsUser(IUser InputUserToSendData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
