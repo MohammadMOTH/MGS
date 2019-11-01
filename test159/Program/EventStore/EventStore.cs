2cc8f3865713a161e4a181bbe1ebccf90c4fa59d
Samir Alholo <samir.alholo.98@gmail.com>
2019-11-01 10:45:03 +0200

    #Creating EventStore Class

---
+++ test159/Program/EventStore/EventStore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ServerGame.Core.EventStore;
+
+namespace ServerGame.Program.EventStore
+{
+    class EventStore : EventStoreAbstract
+    {
+        public override void Parser(IData data)
+        {
+            //Here Parser Code
+        }
+    }
+}
