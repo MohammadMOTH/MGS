876b5f70d9ab5399fec417a97ce4d782f5a8c146
Samir Alholo <samir.alholo.98@gmail.com>
2019-11-01 10:25:35 +0200

    #Adding General(Parent) Event Interface

--- test159/Interface/Event/IEvnet.cs
+++ test159/Interface/Event/IEvnet.cs
@@ -4,10 +4,42 @@ using System.Text;
 
 namespace ServerGame
 {
-  public  interface Ievnet 
+  public interface Ievnet
     {
-        
+        #region Public_var
+        /// <summary>
+        /// Name of Event
+        /// </summary>
+        public string EventName { get; set; }
+        #endregion
 
 
+        #region Suggestion
+        // Mr.Mohammad Please Note The Bottom Code Is Just Suggestion So If It's Not Important You Can Delete It, Thanks
+        // Mr.Mohammad Please Note The Bottom Code Is Just Suggestion So If It's Not Important You Can Delete It, Thanks
+        // Mr.Mohammad Please Note The Bottom Code Is Just Suggestion So If It's Not Important You Can Delete It, Thanks
+
+
+        /// <summary>
+        /// This Event Raising Before sending the message
+        /// </summary>
+        //public event EventHandler<T> BeforeRaising;
+
+        /// <summary>
+        /// This Event Raising After sending the message
+        /// </summary>
+        //public event EventHandler<T> After;
+
+        /// <summary>
+        /// This Method Use For Raising The Event Before sending the message
+        /// </summary>
+        //public void OnBefore(object o);
+
+        /// <summary>
+        /// This Method Use For Raising The Event After sending the message
+        /// </summary>
+        //public void OnAfter(object o);
+
+        #endregion
     }
 }
