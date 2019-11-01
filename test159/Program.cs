67c9c7987b05a7e3d503dcd9ad4e9fe349335853
Samir Alholo <samir.alholo.98@gmail.com>
2019-11-01 10:27:16 +0200

    #Editing Code Based On New Classes

--- test159/Program.cs
+++ test159/Program.cs
@@ -5,16 +5,16 @@ namespace ServerGame
 {
     class Test
     {
-        
         static void Main(string[] args)
         {
-            string MyMessage = "Hello I'am Here, How Are You...?";
+
+            Program.Data.Message MyMessage = new Program.Data.Message("Hello I'am Here, How Are You...?");
             Program.Room.Room room = new Program.Room.Room();
             Event.MessageSender msgSender = new Event.MessageSender();
 
             msgSender.MessageSent += room.OnMessageSent;
-            msgSender.Send(MyMessage);
-            
+            msgSender.OnMessageSent(MyMessage);
+            Console.WriteLine(msgSender.EventName);
         }
     }
 }
