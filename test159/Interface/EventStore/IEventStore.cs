f3e4697c83da505646f3c38648d990d17c7a047b
Samir Alholo <samir.alholo.98@gmail.com>
2019-11-01 10:39:44 +0200

    #Creating EventStore Interface

---
+++ test159/Interface/EventStore/IEventStore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerGame.Interface.EventStore
+{
+    public interface IEventStore
+    {
+        /// <summary>
+        /// List Of All Stored Events
+        /// </summary>
+        public static List<Ievnet> StoredEvent { get; set; }
+
+        /// <summary>
+        /// Method To Parse Inputed Data Parameter Then Fire The Right Event Based On Data
+        /// </summary>
+        /// <param name="data">Which it Get From Connection To Check Which Event Should Raise</param>
+        public void Parser(IData data);
+
+    }
+}
