b58f99f33b16ee0287f6fa41934434c7825e8761
Samir Alholo <samir.alholo.98@gmail.com>
2019-11-01 10:40:45 +0200

    #Creating Message Data Type Class

---
+++ test159/Program/Data/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerGame.Program.Data
+{
+    public class Message:IData
+    {
+        private string _message;
+
+        public string message { get => _message; set => _message = value; }
+
+        public Message(string msg)
+        {
+            this._message = msg;
+        }
+    }
+}
