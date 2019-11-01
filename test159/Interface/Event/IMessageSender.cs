2fd60fb2af29486ed79c3287306b5ced6d24200e
Samir Alholo <samir.alholo.98@gmail.com>
2019-11-01 10:29:20 +0200

    #Creating MessageSender Event Interface

---
+++ test159/Interface/Event/IMessageSender.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ServerGame.Event;
+using ServerGame.Program.Data;
+
+namespace ServerGame.Interface.Event
+{
+    public interface IMessageSender : Ievnet
+    {
+        #region Public_event
+        /// <summary>
+        /// This Event Raising Before sending the message
+        /// </summary>
+        public event EventHandler<MessageSenderEventArgs> MessageSending;
+
+        /// <summary>
+        /// This Event Raising After sending the message
+        /// </summary>
+        public event EventHandler<MessageSenderEventArgs> MessageSent;
+
+        #endregion
+
+        #region Public_Method
+        /// <summary>
+        /// This Method Use For Raising The Event Before sending the message
+        /// </summary>
+        /// <param name="msg">The Message Which Will Send</param>
+        public void OnMessageSending(Message msg);
+
+        /// <summary>
+        /// This Method Use For Raising The Event After sending the message
+        /// </summary>
+        /// /// <param name="msg">The Message Which Will Send</param>
+        public void OnMessageSent(Message msg);
+
+        #endregion
+    }
+}
