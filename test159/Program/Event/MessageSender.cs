bd469f38be435898dc9a3a3d3aa9c15a1241bddc
Samir Alholo <samir.alholo.98@gmail.com>
2019-11-04 19:14:44 +0200

    #Changing NameSpace & Editing MessageSenderArgs

--- test159/Program/Event/MessageSender.cs
+++ test159/Program/Event/MessageSender.cs
@@ -5,19 +5,25 @@ using System.Threading;
 using ServerGame.Core.Event;
 using ServerGame.Program.Data;
 
-namespace ServerGame.Event
+namespace ServerGame.Program.Event
 {
     public class MessageSenderEventArgs : EventArgs
     {
-        public Message message { get; set; }
+        private Message _message;
+        public Message Message { get => _message; set => _message = value; }
+
+        public MessageSenderEventArgs(Message msg)
+        {
+            Message = msg;
+        }
     }
+
     class MessageSender : MessageSenderAbstract
     {
         public MessageSender()
         {
             EventName = "MessageSender";
         }
-        //public override string EventName { get; set; }
 
         public override event EventHandler<MessageSenderEventArgs> MessageSending;
         public override event EventHandler<MessageSenderEventArgs> MessageSent;
@@ -29,8 +35,8 @@ namespace ServerGame.Event
         public override void OnMessageSent(Message msg)
         {
             Console.WriteLine("The Message Sending...");
-            Thread.Sleep(3000);
-            MessageSent?.Invoke(this, new MessageSenderEventArgs() { message = msg });
+            Thread.Sleep(1500);
+            MessageSent?.Invoke(this, new MessageSenderEventArgs(msg));
         }
 
 
