24434bb4f1a0e87d63357248f3e6e631e63b7178
Samir Alholo <samir.alholo.98@gmail.com>
2019-11-04 19:21:22 +0200

    #Building Event Store Class

--- test159/Program/EventStore/EventStore.cs
+++ test159/Program/EventStore/EventStore.cs
@@ -2,14 +2,44 @@
 using System.Collections.Generic;
 using System.Text;
 using ServerGame.Core.EventStore;
+using System.Linq;
+using System.Reflection;
+using ServerGame.Program.Event;
+using ServerGame.Interface.Event;
+
 
 namespace ServerGame.Program.EventStore
 {
     class EventStore : EventStoreAbstract
     {
+        
+        public static IEnumerable<object> GetAllClasses(string nameSpace) 
+        { 
+            Assembly asm = Assembly.GetExecutingAssembly();
+            if (!String.IsNullOrWhiteSpace(nameSpace))
+                return asm.GetTypes().Where(x => x.Namespace == nameSpace).Select(x => x);
+            else return asm.GetTypes().Select(x => x);
+        }
+        
+        public static void PrinAllClass(IEnumerable<object> list)
+        {
+            foreach(object t in list)
+            {
+                //var myObject = typeof(t);
+                Console.WriteLine(t);
+            }
+        }
+        
+
         public override void Parser(IData data)
         {
-            //Here Parser Code
+            //foreach (string c in GetAllClasses("ServerGame.Program.Event"))
+            //{
+            //    if (data == )
+            //    Console.WriteLine("Hello From Parser");
+            //    Console.WriteLine(c);
+            //}
+            
         }
     }
 }
