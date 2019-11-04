d7d81eef56f4a997d426cfe012c8ffbd17c68802
Samir Alholo <samir.alholo.98@gmail.com>
2019-11-04 19:20:33 +0200

    #Creating Generics Event Data Interface

---
+++ test159/Interface/Data/IEventData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerGame.Interface.Data
+{
+    public interface IEventData<T>
+    {
+        /// <summary>
+        /// Data of Event
+        /// </summary>
+        public IEventData<T> Data { get; }
+    }
+}
