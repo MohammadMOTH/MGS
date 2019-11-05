d0d41adca227eb02360267440fae21abfcfd846e
Samir Alholo <samir.alholo.98@gmail.com>
2019-11-05 08:18:27 +0200

    #Building [Parser] Method

--- test159/Program/EventStore/EventStore.cs
+++ test159/Program/EventStore/EventStore.cs
@@ -5,7 +5,7 @@ using ServerGame.Core.EventStore;
 using System.Linq;
 using System.Reflection;
 using ServerGame.Program.Event;
-using ServerGame.Interface.Event;
+using ServerGame.Interface.Data;
 
 
 namespace ServerGame.Program.EventStore
@@ -13,7 +13,7 @@ namespace ServerGame.Program.EventStore
     class EventStore : EventStoreAbstract
     {
         
-        public static IEnumerable<object> GetAllClasses(string nameSpace) 
+        public static IEnumerable<Type> GetAllClasses(string nameSpace) 
         { 
             Assembly asm = Assembly.GetExecutingAssembly();
             if (!String.IsNullOrWhiteSpace(nameSpace))
@@ -21,25 +21,48 @@ namespace ServerGame.Program.EventStore
             else return asm.GetTypes().Select(x => x);
         }
         
-        public static void PrinAllClass(IEnumerable<object> list)
+        public static void PrinAllClass(IEnumerable<Type> list)
         {
-            foreach(object t in list)
+            foreach(Type t in list)
             {
                 //var myObject = typeof(t);
-                Console.WriteLine(t);
+                //Console.WriteLine(t.GetType().GetMethod("OnMessageSending").Name);
+                //var myProp = myclass.get
+                //t.GetType().GetProperty()
+                
+                    //var myclass = t.GetProperty("Data").PropertyType.Name;
+                    if(t.GetProperty("Data") != null)
+                {
+                    var myclass = t.GetProperty("Data").PropertyType.Name;
+                    Console.WriteLine(myclass);
+                }
+                else Console.WriteLine("Bad");
+                    
+                 
+                //var myclass = t.GetProperty("Data").PropertyType.Name;
+
+                //var myclass = typeof(t.Name);
             }
         }
         
 
-        public override void Parser(IData data)
+        public override void Parser(ICustomeData data, IEnumerable<Type> list)
         {
-            //foreach (string c in GetAllClasses("ServerGame.Program.Event"))
-            //{
-            //    if (data == )
-            //    Console.WriteLine("Hello From Parser");
-            //    Console.WriteLine(c);
-            //}
-            
+            foreach (Type currentClass in list)
+            {
+                if (currentClass.GetProperty("Data") != null)
+                {
+                    var DataClass = currentClass.GetProperty("Data").PropertyType.Name;
+                    var DataConnection = data.GetType().Name;
+                    if(DataClass == DataConnection)
+                    {
+                        
+                    }
+                    Console.WriteLine(DataClass);
+                    Console.WriteLine(DataConnection);
+                }
+                else Console.WriteLine("Bad");
+            }
         }
     }
 }
