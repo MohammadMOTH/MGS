8a1930e32ab2b22522d62c14a067d0ce7ce9bf87
Samir Alholo <samir.alholo.98@gmail.com>
2019-11-05 08:16:53 +0200

    #Adding [ICustomeData]

--- test159/Program/Data/Message.cs
+++ test159/Program/Data/Message.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ServerGame.Interface.Data;
 
 namespace ServerGame.Program.Data
 {
-    public class Message:IData
+    public class Message: ICustomeData
     {
         private string _message;
-
         public string message { get => _message; set => _message = value; }
 
         public Message(string msg)
