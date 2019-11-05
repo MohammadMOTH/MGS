dcb509b4ab42afff3edea61da198df676620e0e2
Samir Alholo <samir.alholo.98@gmail.com>
2019-11-05 08:20:04 +0200

    #Editing Code For Testing Parser Method

--- test159/Program.cs
+++ test159/Program.cs
@@ -13,30 +13,19 @@ namespace ServerGame
         static void Main(string[] args)
         {
 
-            Program.Data.Message MyMessage = new Program.Data.Message("Hello I'am Here, How Are You...?");
+            Program.Data.Message MyMessage = new Program.Data.Message("Hello I'am Here, From Message Class");
             Program.Room.Room room = new Program.Room.Room();
             Program.Event.MessageSender msgSender = new Program.Event.MessageSender();
 
             msgSender.MessageSent += room.OnMessageSent;
             msgSender.OnMessageSent(MyMessage);
             
-            Console.WriteLine(".......................................................");
-            Console.WriteLine(".......................................................");
-            Console.WriteLine(".......................................................");
-            Console.WriteLine(".......................................................");
-            
-
             EventStore.PrinAllClass(EventStore.GetAllClasses("ServerGame.Program.Event"));
             
             Console.WriteLine(".......................................................");
-            Console.WriteLine(".......................................................");
-            Console.WriteLine(".......................................................");
-            Console.WriteLine(".......................................................");
-            //EventStore e = new EventStore();
-            //e.Parser(MyMessage);
-            //Type T = Type.GetType("ServerGame.Program.Event");
-            //Type aa = Assembly.GetExecutingAssembly().GetType();
-            //var s = typeof(T);
+
+            EventStore es = new EventStore();
+            es.Parser(MyMessage, EventStore.GetAllClasses("ServerGame.Program.Event"));
         }
     }
 }
