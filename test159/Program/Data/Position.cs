707bff5a3934ee5253d1e085111727236318fdf6
Samir Alholo <samir.alholo.98@gmail.com>
2019-11-05 08:19:08 +0200

    #Creating New Data Type For Testing

---
+++ test159/Program/Data/Position.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ServerGame.Interface.Data;
+
+namespace ServerGame.Program.Data
+{
+    public class Position : ICustomeData
+    {
+        private int _x;
+        private int _y;
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Position(int x,int y)
+        {
+            this._x = x;
+            this._y = y;
+        }
+    }
+}
